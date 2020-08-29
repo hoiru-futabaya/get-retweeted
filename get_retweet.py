9e2a7913d89fb1aa60d08c43208574e7b978941d
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-08-29 20:29:55 +0900

    あとで見て分かるようにコードを書き直した

--- get_retweet.py
+++ get_retweet.py
@@ -1,89 +1,84 @@
-import schedule
-import datetime
-import time
-import os
-import subprocess, ast
-import pprint, json, config #標準のjsonモジュールとconfig.pyの読み込み
-from requests_oauthlib import OAuth1Session #OAuthのライブラリの読み込み
+#モジュールとconfig.pyの読み込み
+import schedule, datetime, time, os, subprocess, ast, pprint, json, config, collections
+#OAuthのライブラリの読み込み
+from requests_oauthlib import OAuth1Session
 
-def get_retweet():
-	hantei = 0 #RT数の総数の変動で通知の有無をみる
-	dict = {}
+#認証処理
+CK = config.Consumer_key
+CS = config.Consumer_secret
+AT = config.Access_token
+ATS = config.Access_secret
+twitter = OAuth1Session(CK, CS, AT, ATS)
 
-	CK = config.Consumer_key
-	CS = config.Consumer_secret
-	AT = config.Access_token
-	ATS = config.Access_secret
-	twitter = OAuth1Session(CK, CS, AT, ATS) #認証処理
-
-	url = "https://api.twitter.com/1.1/statuses/retweets_of_me.json" #タイムライン取得エンドポイント
-
-	params ={'count' : 5} #取得数
-	res = twitter.get(url, params = params)
-	timelines = json.loads(res.text) #レスポンスからタイムラインリストを取得
-	#pprint.pprint()
+#取得制限の変数3つはは事前に宣言しておく必要がある
+limit = 0
+reset = 0
+sec = 0
 
 
-	for line in timelines:
+#retweets_of_me.jsonのときはoptionフラグを1にする
+def get_tweet(url, option = 0):
+  global limit, reset, sec
+  if option == 1:
+    res = twitter.get(url, params = {'count': 5})
 
-		target_id = line['id_str']
-		url2 = "https://api.twitter.com/1.1/statuses/retweets/" + target_id + ".json" #タイムライン取得エンドポイント
+  else:
+    res = twitter.get(url)
 
-		res2 = twitter.get(url2)
-		timelines2 = json.loads(res2.text) #レスポンスからタイムラインリストを取得
+    #取得制限について
+    limit = res.headers['x-rate-limit-remaining']
+    reset = res.headers['x-rate-limit-reset']
+    sec = int(res.headers['X-Rate-Limit-Reset']) - time.mktime(datetime.datetime.now().timetuple()) #UTCを秒数に変換
 
-		if 'errors' in timelines:
-			break
+  raw_loaded_data = json.loads(res.text)
 
-		limit = res2.headers['x-rate-limit-remaining'] #リクエスト可能残数の取得
-		reset = res2.headers['x-rate-limit-reset'] #リクエスト叶残数リセットまでの時間(UTC)
-		sec = int(res2.headers['X-Rate-Limit-Reset']) - time.mktime(datetime.datetime.now().timetuple()) #UTCを秒数に変換
+  return raw_loaded_data, limit, reset, sec
 
-		dict[int(target_id)] = len(timelines2)
 
-		hantei += len(timelines2)
+#リツイートされた自分のツイートを取得
+url = "https://api.twitter.com/1.1/statuses/retweets_of_me.json"
+loaded_data, _, _, _ = get_tweet(url, option=1)
 
-	return hantei, dict, timelines2, limit, sec, twitter
+#RTされたツイートのidでリストを作る
+tweetid = [loaded_data[line]['id'] for line in range(len(loaded_data))]
 
-holder = get_retweet()
+#ツイートごとのRT関連情報を取得
+#(APIを何回も叩きたくないのでres_holderにまとめておく)
+res_holder = []
+for target_id in tweetid:
+  url = "https://api.twitter.com/1.1/statuses/retweets/" + str(target_id) + ".json"
+  loaded_data, limit, reset, sec = get_tweet(url)
+  [res_holder.append(loaded_data[r]) for r in range(len(loaded_data))]
 
-if 'errors' in holder[2]:
-	print(holder[2])
-	print("limit: " + holder[3])
-	print("sec: " + str(holder[4]))
+#比較用のリスト（RTされたツイートのid、重複あり）を作る
+holder = [d['retweeted_status'].get('id') for d in res_holder]
 
-else:
-	with open('holder') as g:
-		before = ast.literal_eval(g.read()) #1個前の辞書データ
-	get = get_retweet()
-	now = get[1]
+#5分前と現在の比較用リストを取得
+with open('holder') as g:
+  before = ast.literal_eval(g.read()) #1個前の辞書データ
+now = holder
 
-	if not now == before:
-		for i in now:
-			url3 = "https://api.twitter.com/1.1/statuses/retweets/" + str(i) + ".json"
-			res3 = get[5].get(url3)
-			timelines3 = json.loads(res3.text) #レスポンスからタイムラインリストを取得
-			rt_users = []
+import pdb; pdb.set_trace() # 追加
+#RT数を比較
+for n in tweetid:
+  RT_count = now.count(n) - before.count(n)
 
-			#import pdb; pdb.set_trace() # 追加
-			if i not in before:
-				for k in range(now[i]):
-					users = timelines3[k]['user']['name']
-					rt_users.append(users)
-				print('Retweeted by ' + str(rt_users) + '\n' + timelines3[0]['retweeted_status']['text'])
+  if RT_count < 1:
+    pass
 
-			elif not now[i] == before[i] :
-				for k in range(now[i]-before[i]):
-					users = timelines3[k]['user']['name']
-					rt_users.append(users)
-				print('Retweeted by ' + str(rt_users) + '\n' + timelines3[0]['retweeted_status']['text'])
-	#定時監視用のデータを別ファイルに控える
-	with open('holder', mode='w') as f:
-		f.write(str(holder[1]))
+  else:
+    users = [res_holder[k]['user']['name'] for k in range(RT_count)]
+    text = res_holder[0]['retweeted_status']['text']
+    print('Retweeted by ' + str(users) + '\n' + text)
 
-#schedule.every(5).minutes.do(job)
+#定時監視用のデータを別ファイルに控える
+with open('holder', mode='w') as f:
+  f.write(str(holder))
 #
-## jobの実行監視、指定時間になったらjob関数を実行
-#while True:
-#        schedule.run_pending()
-#        time.sleep(5)
+##schedule.every(5).minutes.do(job)
+##
+### jobの実行監視、指定時間になったらjob関数を実行
+##while True:
+##        schedule.run_pending()
+##        time.sleep(5)
+
